35d739191964f283b07c7da99ce274ee14809e3e
GParr-Morley <45260541+GParr-Morley@users.noreply.github.com>
2019-02-04 14:59:32 +0000

    Add files via upload
    merged distance check with main file

--- wall_robot.ino
+++ wall_robot.ino
@@ -4,6 +4,26 @@ Stepper left(SPR, 2, 3, 4, 5);
 Stepper right(SPR, 6, 7, 8, 9);
 int stepCount = 0; // how far the step has gone
 
+int trigPin = 8;
+int echoPin = 7;
+long duration;
+
+void distance(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  int distanceCm= duration*0.034/2;
+ 
+  Serial.println (distanceCm);
+  delay(100);
+}
+
 void forwards(int steps,int rpm){
 left.setSpeed(rpm);
 right.setSpeed(rpm);
@@ -12,8 +32,9 @@ right.step(steps);
 
 }
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
