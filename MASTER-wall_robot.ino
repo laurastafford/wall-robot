6e2b64f31ee15acfc854db1eeab51871be3fc2bf
GParr-Morley <45260541+GParr-Morley@users.noreply.github.com>
2019-03-11 14:45:58 +0000

    added jacks lcd and comments

--- MASTER-wall_robot.ino
+++ MASTER-wall_robot.ino
@@ -1,5 +1,12 @@
+//servo setup
 #include <Servo.h>
 Servo myservo;  
+
+//lcd setup
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; 
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+//pins for LCD screen are 12, 11, 5, 4, 3 & 2
 
 int pos = 0;  
 
@@ -43,6 +50,20 @@ analogWrite(motorPinL, speed);
 analogWrite(motorPinR, speed);
 
 }
+//The LCD screen will need to be cleared, 
+//as it will continously print and look wrong &
+//without the delay it will instantally clear
+//(jack and issac)
+
+void Lcd(int text) {
+    lcd.setCursor(0, 0);
+  //sets where the message will be printed
+  lcd.print(text);
+  //Prints a message to the LCD screen
+  delay(500);
+  lcd.clear();
+}
+
 // goes Left and right (Laura and George)
 void left(int speed){
 analogWrite(motorPinL, speed);  
@@ -59,6 +80,7 @@ void setup() {
   pinMode(motorPinL, OUTPUT);
   pinMode(motorPinR, OUTPUT);
   myservo.attach(9); 
+  lcd.begin(16, 2); 
   
 }
 
