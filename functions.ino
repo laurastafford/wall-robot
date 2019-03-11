2321ae0be4c14e41339eed5f693b1dfe47830df6
jdh121 <48320040+jdh121@users.noreply.github.com>
2019-03-11 14:25:15 +0000

    Added the Functions for the LCD

--- functions.ino
+++ functions.ino
@@ -1,6 +1,11 @@
   int trigPin = 8;
   int echoPin = 7;
 
+  #include <LiquidCrystal.h>
+  const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; 
+  LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+  //pins for LCD screen are 12, 11, 5, 4, 3 & 2
+
   long duration;
 
 void distance(){
@@ -19,13 +24,24 @@ void distance(){
   delay(100);
 }
 
-
+void Lcd(int text) {
+    lcd.setCursor(0, 0);
+  //sets where the message will be printed
+  lcd.print(text);
+  //Prints a message to the LCD screen
+  delay(500);
+  lcd.clear();
+}
+//The LCD screen will need to be cleared, 
+//as it will continously print and look wrong &
+//without the delay it will instantally clear
 
 void setup() {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-
+  lcd.begin(16, 2); 
+  //Allows the LCD screen to function
 }
 
 void setup() {
